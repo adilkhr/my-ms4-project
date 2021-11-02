78801e051407c797f7586f8d36b9c0b4e20b7faa
adilkhr <adilkhair99@gmail.com>
2021-11-02 13:18:23 +0000

    refactoring python code errors

--- contact/views.py
+++ contact/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from .models import Contact
-from django.http import HttpResponse
+
 
 # Create your views here.
 def contact(request):
     """ A view to return the contact page """
-    if request.method=="POST":
-        contact=Contact()
-        name=request.POST.get('name')
-        email=request.POST.get('email')
-        subject=request.POST.get('subject')
-        contact.name=name
-        contact.email=email
-        contact.subject=subject
+    if request.method == "POST":
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        contact.name = name
+        contact.email = email
+        contact.subject = subject
         contact.save()
         return render(request, 'contact/thankyou.html')
     return render(request, 'contact/contact.html')
